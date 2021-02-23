54b7082bd5619853d362c3e1cd4fd93e818322f3
李俊諭 JYUN-YU LI <s123600g@gmail.com>
2021-02-23 22:39:03 +0800

    feat : 上傳簡單練習範例內容

---
+++ DotnetCore_Console_.NetGeneric-Host_Sample/DotnetCore_Console_.NetGeneric-Host_Sample/AppTask.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetCore_Console_.NetGeneric_Host_Sample
+{
+    /// <summary>
+    /// 建立一個App，並繼承IHostedService介面
+    /// 並實作工作起始函式與結束函式內容
+    /// </summary>
+    public class AppTask : IHostedService
+    {
+        #region Variables
+
+        private readonly ILogger<AppTask> logger;
+        private readonly IHostApplicationLifetime appLifetime;
+
+        #endregion Variables
+
+        #region Constructor
+
+        public AppTask(
+            ILogger<AppTask> _logger,
+            IHostApplicationLifetime _appLifetime
+        )
+        {
+            logger = _logger;
+            appLifetime = _appLifetime;
+        }
+
+        #endregion Constructor
+
+        /// <summary>
+        /// App工作開始進入點階段
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                // 在這裡實作App 內容
+                logger.LogInformation("App running at: {time}", DateTimeOffset.Now);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Unhandled exception!");
+            }
+            finally
+            {
+                // 不管有沒有錯誤，最後都要宣告當前工作已經結束，進入工作結束進入點階段
+                appLifetime.StopApplication();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// App工作結束進入點階段
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("App stopped at: {time}", DateTimeOffset.Now);
+
+            // 告訴上層程式當前工作已經結束完成
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DotnetCore_Console_.NetGeneric-Host_Sample/DotnetCore_Console_.NetGeneric-Host_Sample/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DotnetCore_Console_.NetGeneric_Host_Sample
+{
+    /**
+     * 參考 使用 .NET Generic Host 建立 Console 主控台應用程式 (.NET Core 3.1+)
+     * https://blog.miniasp.com/post/2020/12/08/NET-Generic-Host-Build-Console-App
+     */
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // 取得 IHostBuilder 物件實體
+            IHostBuilder hostBuilder = CreateHostBuilder(args);
+            // 取得 IHost物件實體
+            IHost host = hostBuilder.Build();
+
+            // 透過IHost啟動Host應用程式去執行App
+            host.Run();
+        }
+
+        /**
+         * 關於LogLevel
+         * https://docs.microsoft.com/zh-tw/dotnet/api/microsoft.extensions.logging.loglevel?view=dotnet-plat-ext-5.0&WT.mc_id=DT-MVP-4015686
+         * 透過Appsettings內設置最小輸出顯示紀錄層級
+         * Host.CreateDefaultBuilder(args) 預設就有包含設定載入 appsettings.json
+         */
+
+        /// <summary>
+        /// 實作建立IHost物件配置
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+        {
+            // 注入一個App 服務
+            services.AddHostedService<AppTask>();
+        });
+    }
+}
